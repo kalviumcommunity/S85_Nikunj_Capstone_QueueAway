bd6771472d4bc6561d595430d6600cfd37ae9246
nikunj-kohli <nikunj.kohli.s85@kalvium.community>
2025-04-27 20:26:17 +0530

    POST API USAGE

--- backend/routes/shopRoutes.js
+++ backend/routes/shopRoutes.js
@@ -2,4 +2,5 @@ const express = require('express');
 const { getAllShops, createShop } = require('../controllers/shopController');
 const router = express.Router();
 router.get('/', getAllShops);
+router.post('/', createShop);
 module.exports = router;
