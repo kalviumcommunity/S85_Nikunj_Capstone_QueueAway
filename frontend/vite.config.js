31cc21872efdb2b4e241e2b8376f706655b75c2c
nikunj-kohli <nikunj.kohli.s85@kalvium.community>
2025-04-30 10:49:45 +0530

    added backend link vite.config.js for deployement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': 'https://s85-nikunj-capstone-queueaway.onrender.com/'
     }
   }
 });
